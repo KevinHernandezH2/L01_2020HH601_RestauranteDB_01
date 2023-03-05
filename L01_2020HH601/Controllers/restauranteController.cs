c2ac6478b4658c348a8dd2ba30ef1481a48875fb
KevinHernandezH2 <kevin.hernandez8@catolica.edu.sv>
2023-03-04 22:46:54 -0600

    ENTREGA FINAL

---
+++ L01_2020HH601/Controllers/restauranteController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L01_2020HH601.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class restauranteController : ControllerBase
+    {
+    }
+}
---
+++ L01_2020HH601/Models/restaurante.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01_2020HH601.Models
+{
+    public class restaurante
+    {
+        
+    public class clientes
+        {
+            [Key]
+            public int clienteid { get; set; }
+            public string nombreCliente { get; set; }
+            public string direccion { get; set; }
+
+        }
+
+        public class pedidos
+        {
+            [Key]
+            public int pedidosId { get; set; }
+            public int motoristaId { get; set; }
+            public int clienteId { get; set; }
+            public int platoId { get; set; }
+            public int cantidad { get; set; }
+            public decimal precio { get; set; }
+
+        }
+
+        public class platos
+        {
+            [Key]
+            public int platoId { get; set; }
+            public string nombrePlato { get; set; }
+            public decimal precio { get; set; }
+        }
+        public class motoristas
+        {
+            [Key]
+            public int motoristaId { get; set; }
+            public string nombreMotorista { get; set; }
+        }
+    }
+}
+
